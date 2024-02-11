a5b22d9eeb42951fed08fce2f60e28d630e183b5
danish-sartaj9000 <154250739+danish-sartaj9000@users.noreply.github.com>
2024-02-12 00:59:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+const number= prompt("Please enter first number ", "between 1 to 10");
+const numberone= prompt("Please enter second number ", "between 1 to 10");
+
+let sum = parseInt(number) + parseInt(numberone);
+console.log(sum)
+// var x = 8;
+// var y = 6;
+// z = x + y
+// document.write(hello )
+
+
